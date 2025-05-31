26ea8b539f59422d6caaef3ae9804870ad3ebfe6
Nabath Nuur <muhammadnabathnuur@gmail.com>
2025-05-31 20:50:51 +0700

    Update Sphere.java

--- models/Sphere.java
+++ models/Sphere.java
@@ -31,7 +31,7 @@ public class Sphere extends Shape {
         System.out.printf("Luas permukaan  : %.2f\n", getSurfaceArea());
         System.out.printf("Massa           : %.2f\n", getMass());
         System.out.printf("Massa dalam kg  : %.2f\n", gramToKilogram((int) getMass()));
-        System.out.printf("Biaya kirim     : Rp %d\n", (int) calculateCost(getMass()));
+        System.out.printf("Biaya kirim     : Rp %d\n", (int) calculateCost(gramToKilogram((int) getMass())));
     }
     
     @Override
